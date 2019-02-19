b320ac9bbedc7dd847c3c81f6d9cf7c170a84e18
Boo Kai Hsien <kaihsien.boo@visenze.com>
2019-02-20 01:04:32 +0800

    final polish for today

--- src/main.py
+++ src/main.py
@@ -1,36 +1,51 @@
+import sys
+import numpy as np
+
 from UltimateTicTacToeAI import UCT
 from UltimateTicTacToeLogic import GameState
 
 
-def UCTPlayGame():
+def UCTPlayGame(itermax):
     """ Play a sample game between two UCT players where each player gets a different number 
         of UCT iterations (= simulations = tree nodes).
     """
     print("Welcome to Ultimate Tic-Tac-Toe!")
-    player = 1 if input("Do you want to go first? [Y/N]: ") == "Y" else 2
-    print()
+    player = 2 if input("Do you want to go first? [Y/N]: ") == "N" else 1
 
-    state = GameState() # uncomment to play OXO
-    while (state.GetMoves() != []):
-        currentPlayer = 3 - state.playerJustMoved
-        print("Moves for player " + str(currentPlayer) + ": " + str(state.GetMoves()) + "\n")
-        if currentPlayer == player:
-            # m = UCT(rootstate = state, itermax = 2000, verbose = False) # play with values for itermax and verbose = True
-            m = int(input("Enter the cell you want to play: "))
-        else:
-            m = UCT(rootstate = state, itermax = 7000, verbose = False)
-            print("Opponent played: " + str(m) + "\n")
-        state.DoMove(m)
+    state = GameState()
+    while state.GetMoves():
+        currentPlayer = state.NextPlayer()
+
         print(str(state))
-    if state.GetResult(state.playerJustMoved) == 1.0:
+        print("Moves for player " + str(currentPlayer) + ": ")
+        print(np.matrix(state.GetMoves()), "\n")
+
+        if currentPlayer == player:
+            while m not in state.GetMoves():
+                try:
+                    m = int(input("Your move: "))
+                except ValueError:
+                    continue
+            # m = random.choice(state.GetMoves())
+        else:
+            m = UCT(rootstate=state, itermax=itermax, verbose=False)
+            print("AI played: " + str(m))
+        state.DoMove(m)
+    print(str(state))
+
+    if state.GetResult(state.playerJustMove) == 1.0:
         print("Player " + str(state.playerJustMoved) + " wins!")
+        return state.playerJustMoved
     elif state.GetResult(state.playerJustMoved) == 0.0:
-        print("Player " + str(3 - state.playerJustMoved) + " wins!")
-    else: print("Nobody wins!")
+        print("Player " + str(state.NextPlayer()) + " wins!")
+        return state.NextPlayer()
+    else:
+        print("Nobody wins!")
+        return 0
+
 
 if __name__ == "__main__":
-    """ Play a single game to the end using UCT for both players. 
-    """
-    UCTPlayGame()
 
-            
+    itermax = int(sys.argv[1])
+
+    UCTPlayGame(itermax)
