043121a9eaf1f5f24390115a13b0da23b96e7274
Boo Kai Hsien <kaihsien.boo@visenze.com>
2019-02-19 23:47:08 +0800

    refactor

--- src/UltimateTicTacToeLogic.py
+++ src/UltimateTicTacToeLogic.py
@@ -1,21 +1,6 @@
 from copy import deepcopy
 
 
-def board(move):
-    return move // 9
-
-def cell(move):
-    return move % 9
-
-def toMove(board, cell):
-    return 9 * board + cell
-
-def Winner(miniboard):
-    for (x,y,z) in [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]:
-        if (miniboard[x] == miniboard[y] == miniboard[z]) and (miniboard[z] != 0):
-            return miniboard[x]
-    return 0
-
 class GameState:
     """ A state of the game, i.e. the game board. These are the only functions which are
         absolutely necessary to implement UCT in any 2-player complete information deterministic
@@ -24,9 +9,23 @@ class GameState:
         By convention the players are numbered 1 and 2.
     """
 
+    WINNING_COMBINATIONS = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+
+    @classmethod
+    def Board(cls, move):
+        return move // 9
+
+    @classmethod
+    def Position(cls, move):
+        return move % 9
+
+    @classmethod
+    def ToMove(cls, board, cell):
+        return board * 9 + cell
+
     def __init__(self):
         self.playerJustMoved = 2  # At the root pretend the player just moved is player 2 - player 1 has the first move
-        self.activeBoard = -1
+        self.activeBoard = None
         self.pieces = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -37,7 +36,7 @@ class GameState:
                        [0, 0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0, 0]]
         self.winnerOfBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.winner = 0
+        self.winnerOfGame = 0
 
     def Clone(self):
         """ Create a deep clone of this game state.
@@ -47,54 +46,63 @@ class GameState:
         st.activeBoard = self.activeBoard
         st.pieces = deepcopy(self.pieces)
         st.winnerOfBoard = deepcopy(self.winnerOfBoard)
-        st.winner = self.winner
+        st.winnerOfGame = self.winnerOfGame
         return st
+
+    def NextPlayer(self):
+        return 3 - self.playerJustMoved
+
+    def GetWinner(self, boardID):
+        board = self.pieces[boardID]
+        for (x, y, z) in GameState.WINNING_COMBINATIONS:
+            if board[x] == board[y] == board[z] != 0:
+                return board[x]
+        return 0
 
     def DoMove(self, move):
         """ Update a state by carrying out the given move.
             Must update playerJustMoved.
         """
-        self.playerJustMoved = 3 - self.playerJustMoved
-        self.pieces[board(move)][cell(move)] = self.playerJustMoved
-        self.activeBoard = cell(move)
+        board = GameState.Board(move)
+        position = GameState.Position(move)
 
-        maybe = False
-        if Winner(self.pieces[board(move)]) == self.playerJustMoved:
-            self.winnerOfBoard[board(move)] = self.playerJustMoved
-            maybe = True
+        self.playerJustMoved = self.NextPlayer()
+        self.pieces[board][position] = self.playerJustMoved
+        self.activeBoard = position
 
-        if maybe:
-            for (x, y, z) in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]:
-                if (self.winnerOfBoard[x] == self.winnerOfBoard[y] == self.winnerOfBoard[z]) and (self.winnerOfBoard[z] != 0):
-                    self.winner = self.winnerOfBoard[z]
+        if self.GetWinner(board) == self.playerJustMoved:
+            self.winnerOfBoard[board] = self.playerJustMoved
+            for (x, y, z) in GameState.WINNING_COMBINATIONS:
+                if self.winnerOfBoard[x] == self.winnerOfBoard[y] == self.winnerOfBoard[z] != 0:
+                    self.winnerOfGame = self.winnerOfBoard[z]
 
     def GetMoves(self):
         """ Get all possible moves from this state.
         """
         # No moves when someone wins
-        if self.winner != 0:
+        if self.winnerOfGame != 0:
             return []
 
-        if self.activeBoard != -1 and self.winnerOfBoard[cell(self.activeBoard)] == 0:
-            return [toMove(self.activeBoard, i) for i in range(9) if self.pieces[cell(self.activeBoard)][i] == 0]
-        else:
-            moves = []
-            for i in range(9):
-                if self.winnerOfBoard[i] == 0:
-                    moves.extend([toMove(i, j) for j in range(9) if self.pieces[i][j] == 0])
-            return moves
+        if self.activeBoard is not None and self.winnerOfBoard[self.activeBoard] == 0:
+            moves = [GameState.ToMove(self.activeBoard, i) for i in range(9) if self.pieces[self.activeBoard][i] == 0]
+            if moves:
+                return moves
 
-    def GetResult(self, playerjm):
-        """ Get the game result from the viewpoint of playerjm.
+        moves = []
+        for i in range(9):
+            if self.winnerOfBoard[i] == 0:
+                moves.extend([GameState.ToMove(i, j) for j in range(9) if self.pieces[i][j] == 0])
+        return moves
+
+    def GetResult(self, playerJustMoved):
+        """ Get the game result from the viewpoint of playerJustMoved.
         """
-        for (x, y, z) in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]:
-            if self.winnerOfBoard[x] == self.winnerOfBoard[y] == self.winnerOfBoard[z]:
-                if self.winnerOfBoard[x] == playerjm:
-                    return 1.0
-                else:
-                    return 0.0
-        if self.GetMoves() == []: return 0.5  # draw
-        assert False
+        if self.winnerOfGame == playerJustMoved:
+            return 1.0
+        elif self.winnerOfGame == 0 and not self.GetMoves():
+            return 0.5
+        else:
+            return 0.0
 
     def __repr__(self):
         """ Don't need this - but good style.
